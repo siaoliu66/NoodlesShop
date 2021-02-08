0c16baa05fb8bf0a0fe1d0ef9308ee92c9846d15
liu <siaoliu66@gmail.com>
2021-02-08 14:09:51 +0800

    建立vue-cli環境

---
+++ src/components/front/index.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div class="header">
+            <h1><a href="#" class="logo">牛老大</a></h1>
+            <ul class="menubar">
+                <li><a href="javascript:;">最新消息</a></li>
+                <li><a href="javascript:;">關於牛老大</a>
+                    <ul class="menubar-list">
+                        <li><a href="javascript:;">經典歷史</a></li>
+                        <li><a href="javascript:;">得獎紀錄</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="menu.html">Menu</a>
+                    <ul class="menubar-list">
+                        <li><a href="menu.html#kind1">麵食</a></li>
+                        <li><a href="menu.html#kind2">湯品</a></li>
+                        <li><a href="menu.html#kind3">水餃</a></li>
+                        <li><a href="menu.html#kind4">小菜滷味</a></li>
+                    </ul>
+                </li>
+                <li><a href="javascript:;">聯絡我們</a></li>
+            </ul>
+            <a href="#" class="showmenu"></a>
+            <div class="clearfix"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  
+}
+</script>
