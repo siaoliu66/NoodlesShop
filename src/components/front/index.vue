53af7bf94922085e4961e9bd0cfc611ebe667c81
liu <siaoliu66@gmail.com>
2021-02-08 16:45:14 +0800

    新增前台畫面、載入swiper

--- src/components/front/index.vue
+++ src/components/front/index.vue
@@ -1,28 +1,14 @@
 <template>
     <div>
+        <intro-swiper></intro-swiper>
         <div class="header">
             <h1><a href="#" class="logo">牛老大</a></h1>
             <ul class="menubar">
-                <li><a href="javascript:;">最新消息</a></li>
-                <li><a href="javascript:;">關於牛老大</a>
-                    <ul class="menubar-list">
-                        <li><a href="javascript:;">經典歷史</a></li>
-                        <li><a href="javascript:;">得獎紀錄</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a href="menu.html">Menu</a>
-                    <ul class="menubar-list">
-                        <li><a href="menu.html#kind1">麵食</a></li>
-                        <li><a href="menu.html#kind2">湯品</a></li>
-                        <li><a href="menu.html#kind3">水餃</a></li>
-                        <li><a href="menu.html#kind4">小菜滷味</a></li>
-                    </ul>
-                </li>
-                <li><a href="javascript:;">聯絡我們</a></li>
+                <li><a href="#">關於牛老大</a></li>
+                <li><a href="#">最新消息</a></li>
+                <li><a href="#">美味菜單</a></li>
+                <li><a href="#">商品管理</a></li>
             </ul>
-            <a href="#" class="showmenu"></a>
-            <div class="clearfix"></div>
         </div>
     </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import swiper, { Navigation, Pagination, Autoplay } from 'swiper'
+// import style (>= Swiper 6.x)
+import 'swiper/swiper-bundle.css'
 
+Vue.use(VueAwesomeSwiper, /* { default options with global component } */)
+swiper.use([Navigation, Pagination, Autoplay])
+
+import introswiper from './components/swiper.vue';
+Vue.component('intro-swiper', introswiper);
 Vue.config.productionTip = false
+
 
 /* eslint-disable no-new */
 new Vue({
